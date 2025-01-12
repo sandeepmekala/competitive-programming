95ca0c8c00bc7db323258c36b6678f944202abf1
Sandeep Mekala <sandeep.mekala@sap.com>
2025-01-12 15:14:25 +0530

    start py

---
+++ coding/src/com/Test.java
@@ -0,0 +1,12 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        Test obj = new Test();
+        System.out.println("Hello World");   
+    }
+
+}
