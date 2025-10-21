cb8932555620767e0ecdc80a84aa271e72f95a29
Sandeep Mekala <sandeep.mekala@sap.com>
2025-10-21 17:16:20 +0530

    rename classes

---
+++ coding/src/com/java/concurrency/04_locks/ReentrantLockExample.java
@@ -0,0 +1,79 @@
+package com.java.concurrency.locks;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
+public class ReentrantLockExample {
+
+	public static void main(String[] args) {
+		ReentrantLockExample obj = new ReentrantLockExample();
+		obj.execute();
+	}
+
+	private void execute() {
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		IntStream.range(0, 10000)
+		.forEach(i -> executor.submit(this::increment));
+		System.out.println(count);
+		stop(executor);
+
+
+		ExecutorService executor2 = Executors.newFixedThreadPool(2);
+		ReentrantLock lock = new ReentrantLock();
+		executor2.submit(() -> {
+			lock.lock();
+			try{
+				sleep(1);
+			}finally{
+				lock.unlock();
+			}
+		});
+
+		executor2.submit(() ->{
+		    System.out.println("Locked: " + lock.isLocked());
+		    System.out.println("Held by me: " + lock.isHeldByCurrentThread());
+		    boolean locked = lock.tryLock();					//when thread calls lock method, thread will be paused. But tryLock will not block the current thread.
+		    System.out.println("Lock acquired: " + locked);
+		});
+		stop(executor2);
+	}
+
+	int count = 0;
+	ReentrantLock lock = new ReentrantLock();
+	void increment() {
+		lock.lock();
+		try{
+			count = count + 1;
+		}finally{
+			lock.unlock();
+		}
+	}
+
+	public void stop(ExecutorService executor) {
+        try {
+            executor.shutdown();
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            System.err.println("termination interrupted");
+        }
+        finally {
+            if (!executor.isTerminated()) {
+                System.err.println("killing non-finished tasks");
+            }
+            executor.shutdownNow();
+        }
+    }
+
+	public void sleep(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
---
+++ coding/src/com/java/concurrency/04_locks/StampedLockExample.java
@@ -0,0 +1,133 @@
+package com.java.concurrency.locks;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.StampedLock;
+
+public class StampedLockExample {
+
+	public static void main(String[] args) {
+		StampedLockExample obj = new StampedLockExample();
+		obj.execute();
+	}
+
+	int count = 0;
+	private void execute() {
+		// StampedLock don't implement reentrant characteristics
+		// Returns stamp, which can be used to unlock and lock validity
+		// Supports another kind of locking mode "optimistic locking"
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		Map<String, String> map = new HashMap<>();
+		StampedLock lock = new StampedLock();
+
+		executor.submit(() -> {
+			long stamp = lock.writeLock();
+			try {
+				sleep(1);
+				map.put("foo", "bar");
+			} finally {
+				lock.unlockWrite(stamp);
+			}
+		});
+
+		Runnable readTask = () -> {
+			long stamp = lock.readLock();
+			try {
+				System.out.println(map.get("foo"));
+				sleep(1);
+			} finally {
+				lock.unlockRead(stamp);
+			}
+		};
+
+		executor.submit(readTask);
+		executor.submit(readTask);
+
+		stop(executor);
+
+		System.out.println();
+		ExecutorService executor2 = Executors.newFixedThreadPool(2);
+		StampedLock lock2 = new StampedLock();
+
+		// If already write lock is available, stamp will be 0
+		// In contrast to normal read lock optimistic read lock doesn't block other
+		// threads to obtain write lock
+		// If other threads aquires write lock, optimistic read lock will become invalid
+		executor2.submit(() -> {
+			long stamp = lock2.tryOptimisticRead();
+			try {
+				System.out.println("Optimistic Lock Valid: " + lock2.validate(stamp));
+				sleep(1);
+				System.out.println("Optimistic Lock Valid: " + lock2.validate(stamp));
+				sleep(2);
+				System.out.println("Optimistic Lock Valid: " + lock2.validate(stamp));
+			} finally {
+				lock2.unlock(stamp);
+			}
+		});
+
+		executor2.submit(() -> {
+			long stamp = lock2.writeLock();
+			try {
+				System.out.println("Write Lock acquired");
+				sleep(2);
+			} finally {
+				lock2.unlock(stamp);
+				System.out.println("Write done");
+			}
+		});
+
+		stop(executor2);
+
+		System.out.println();
+		ExecutorService executor3 = Executors.newFixedThreadPool(2);
+		StampedLock lock3 = new StampedLock();
+
+		count = 0;
+		executor3.submit(() -> {
+			long stamp = lock3.readLock();
+			try {
+				if (count == 0) {
+					stamp = lock3.tryConvertToWriteLock(stamp);
+					if (stamp == 0L) {
+						System.out.println("Could not convert to write lock");
+						stamp = lock3.writeLock();
+					}
+					count = 23;
+				}
+				System.out.println(count);
+			} finally {
+				lock3.unlock(stamp);
+			}
+		});
+
+		stop(executor3);
+	}
+
+	public void stop(ExecutorService executor) {
+        try {
+            executor.shutdown();
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            System.err.println("termination interrupted");
+        }
+        finally {
+            if (!executor.isTerminated()) {
+                System.err.println("killing non-finished tasks");
+            }
+            executor.shutdownNow();
+        }
+    }
+
+	public void sleep(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
